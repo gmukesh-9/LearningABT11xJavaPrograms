4f377dd5a0e4dc4073fcb8fb18b7ac039d1d0b27
Dell <itspecialist09@gmail.com>
2025-04-07 16:33:50 +0530

    7th_April_2025

---
+++ src/ex_01_Java_Basic/HelloWorld.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!!!");
+    }
+}
