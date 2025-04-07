cfa3c48b5c86853fbbce22cbe1ebe6bf75aea51a
Dell <itspecialist09@gmail.com>
2025-04-07 16:35:31 +0530

    7th_April_2025_2

--- src/ex_01_Java_Basic/lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/lab001_HelloWorldProgram.java
@@ -2,6 +2,7 @@ package ex_01_Java_Basic;
 
 public class lab001_HelloWorldProgram {
     public static void main(String[] args) {
-        System.out.println();
+
+        System.out.println("Lab Hello World!!!");
     }
 }
